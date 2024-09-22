ebb8d9aecdd2d4f15b725eedede25fa51b5fe500
Filip Strajnar <filip.strajnar@gmail.com>
2024-09-22 11:37:14 +0200

    Moved pin configuration into PinService.

--- Proculite.RelayControl/Controllers/HomeController.cs
+++ Proculite.RelayControl/Controllers/HomeController.cs
@@ -1,44 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Proculite.RelayControl.Models;
+using Proculite.RelayControl.Services;
 
 namespace Proculite.RelayControl.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly IConfiguration _configuration;
-    private readonly KeyPinControl[] _keyPinControl;
-    private readonly Dictionary<string, Pin[]> _pinControlMap;
+    public readonly PinService _pinService;
 
-    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    public HomeController(ILogger<HomeController> logger, PinService pinService)
     {
         _logger = logger;
-        _configuration = configuration;
-        _keyPinControl = configuration.GetSection("KeyPinControl")
-            .GetChildren()
-            .Select(keyPinControl => new KeyPinControl{
-                Key = keyPinControl.GetSection("Key").Get<string>() ?? "",
-                Pins = keyPinControl.GetSection("Pins").GetChildren().Select(pin => new Pin{
-                    Name = pin.GetSection("Name").Get<string>() ?? "",
-                    Number = pin.GetSection("Number").Get<int>() 
-                }).ToArray()
-            })
-            .ToArray();
-        _pinControlMap = _keyPinControl.ToDictionary(pinControl => pinControl.Key, pinControl => pinControl.Pins);
-    }
-
-    public Pin[] AccessiblePins(HttpRequest httpRequest)
-    {
-        string? keyCookie = httpRequest.Cookies["key"];
-        bool keyExists = keyCookie is not null && _pinControlMap.ContainsKey(keyCookie);
-        Pin[] controlledPins = keyExists ? _pinControlMap[keyCookie] : Array.Empty<Pin>();
-        return controlledPins;
+        _pinService = pinService;
     }
 
     public IActionResult Index()
     {
-        return View(AccessiblePins(Request));
+        return View(_pinService.AccessiblePins(Request));
     }
 
     [HttpPost]
--- Proculite.RelayControl/Services/PinService.cs
+++ Proculite.RelayControl/Services/PinService.cs
@@ -1,12 +1,35 @@
+using Proculite.RelayControl.Models;
+
 namespace Proculite.RelayControl.Services
 {
     public class PinService
     {
-        public PinService()
-        {
+        private readonly IConfiguration _configuration;
+        private readonly KeyPinControl[] _keyPinControl;
+        private readonly Dictionary<string, Pin[]> _pinControlMap;
 
+        public PinService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _keyPinControl = configuration.GetSection("KeyPinControl")
+                .GetChildren()
+                .Select(keyPinControl => new KeyPinControl{
+                    Key = keyPinControl.GetSection("Key").Get<string>() ?? "",
+                    Pins = keyPinControl.GetSection("Pins").GetChildren().Select(pin => new Pin{
+                        Name = pin.GetSection("Name").Get<string>() ?? "",
+                        Number = pin.GetSection("Number").Get<int>() 
+                    }).ToArray()
+                })
+                .ToArray();
+            _pinControlMap = _keyPinControl.ToDictionary(pinControl => pinControl.Key, pinControl => pinControl.Pins);
         }
 
-        
+        public Pin[] AccessiblePins(HttpRequest httpRequest)
+        {
+            string? keyCookie = httpRequest.Cookies["key"];
+            bool keyExists = keyCookie is not null && _pinControlMap.ContainsKey(keyCookie);
+            Pin[] controlledPins = keyExists ? _pinControlMap[keyCookie] : Array.Empty<Pin>();
+            return controlledPins;
+        }
     }
 }
